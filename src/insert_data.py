01b635e699cea6046f8eef913008dde780ff7d06
dongguaguaguagua <57585727+dongguaguaguagua@users.noreply.github.com>
2024-09-11 23:58:40 +0800

    add books interests and related medias

--- src/insert_data.py
+++ src/insert_data.py
@@ -20,12 +20,33 @@ def insert_books(db_path, data):
         card_subtitle = subject['card_subtitle']
         book_subtitle = subject['book_subtitle']
         press = ', '.join(subject['press'])
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         cursor.execute('''
         INSERT OR REPLACE INTO books (id, title, rating, rating_count, pubdate, pages, cover_url, sharing_url, url, author, card_subtitle, book_subtitle, press, update_time)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (id, title, rating, rating_count, pubdate, pages, cover_url, sharing_url, url, author, card_subtitle, book_subtitle, press, now))
+        ''', (id, title, rating, rating_count, pubdate, pages, cover_url, sharing_url, url, author, card_subtitle, book_subtitle, press, update_time))
+
+    conn.commit()
+    conn.close()
+
+
+def insert_small_books(db_path, data):
+    conn = sqlite3.connect(str(db_path))
+    cursor = conn.cursor()
+
+    for subject in data['subjects']:
+        id = subject['id']
+        title = subject['title']
+        rating = subject['rating']['value']
+        rating_count = subject['rating']['count']
+        url = subject['url']
+        update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        cursor.execute('''
+        INSERT OR REPLACE INTO books (id, title, rating, rating_count, url, update_time)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''', (id, title, rating, rating_count, url, update_time))
 
     conn.commit()
     conn.close()
@@ -50,13 +71,60 @@ def insert_movies(db_path, data):
         url = subject['url']
         directors = ', '.join([director['name'] for director in subject['directors']])
         actors = ', '.join([actor['name'] for actor in subject['actors']])
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         cursor.execute('''
         INSERT OR REPLACE INTO movies (id, title, rating, rating_count, pubdate, year, genres, durations, cover_url, sharing_url, countries, url, directors, actors, update_time)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (id, title, rating, rating_count, pubdate, year, genres, durations, cover_url, sharing_url, countries, url, directors, actors, now))
+        ''', (id, title, rating, rating_count, pubdate, year, genres, durations, cover_url, sharing_url, countries, url, directors, actors, update_time))
 
     conn.commit()
     conn.close()
 
+def insert_doulists(db_path, data):
+    conn = sqlite3.connect(str(db_path))
+    cursor = conn.cursor()
+
+    for subject in data['doulists']:
+        id = subject['id']
+        title = subject['title']
+        cover_url = subject['cover_url']
+        sharing_url = subject['sharing_url']
+        url = subject['url']
+        items_count = subject['items_count']
+        followers_count = subject['followers_count']
+        owner_id = subject['owner']['id']
+        owner_uid = subject['owner']['uid']
+        update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        cursor.execute('''
+        INSERT OR REPLACE INTO doulists (id, title, cover_url, sharing_url, url, items_count, followers_count, owner_id, owner_uid, update_time)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (id, title, cover_url, sharing_url, url, items_count, followers_count, owner_id, owner_uid, update_time))
+
+    conn.commit()
+    conn.close()
+
+
+def insert_interests(db_path, data):
+    conn = sqlite3.connect(str(db_path))
+    cursor = conn.cursor()
+
+    for subject in data['interests']:
+        id = subject['id']
+        comment = subject['comment']
+        rating = subject['rating']['value'] if subject['rating'] != None else None
+        sharing_url = subject['sharing_url']
+        create_time = subject['create_time']
+        user_id = subject['user']['id']
+        user_gender = subject['user']['gender']
+        user_name = subject['user']['name']
+        update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        cursor.execute('''
+        INSERT OR REPLACE INTO interests (id, comment, rating, sharing_url, create_time, user_id, user_gender, user_name, update_time)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (id, comment, rating, sharing_url, create_time, user_id, user_gender, user_name, update_time))
+
+    conn.commit()
+    conn.close()
--- src/movies_crawler.py
+++ src/movies_crawler.py
@@ -1,6 +1,4 @@
 import requests
-import sqlite3
-import time
 from utilities import *
 from get_headers import *
 from get_signature import *
@@ -12,47 +10,19 @@ def recommand_data(config, round):
     start = round * int(config['data_count'])
 
     base_url = 'https://frodo.douban.com/api/v2/noviciate/mark_recommendations'
-
     custom_params = {
         'count': config['data_count'],
         'start': str(start),
         'kind': 'movie'
     }
-    base_params = {
-        'apikey': get_apikey(),
-        'channel': config['channel'],
-        'udid': config['udid'],
-        's_rom': config['os_rom'],
-        'timezone': config['timezone']
-    }
-    base_url = add_url_params(base_url, custom_params)
-    base_url = add_url_params(base_url, base_params)
-
-    timestamp = int(time.time())
-    secret_key = get_secret_key()
-    auth_params = {
-        '_ts': str(timestamp),
-        '_sig': get_signature(
-            base_url,
-            config['access_token'],
-            secret_key,
-            timestamp
-        )
-    }
-    url = add_url_params(base_url, auth_params)
-
+    url = build_url(base_url, custom_params)
     response = requests.get(url, headers=headers)
-
     data = response.json()
     save_data(data, 'latest_movie.json')
 
-    # 连接到SQLite数据库（如果数据库不存在，则会自动创建）
-    conn = sqlite3.connect('movies.db')
-    cursor = conn.cursor()
-
     init_movie_table('movies.db')
 
-    total_movie_before = get_total_movie_count('movies.db')
+    total_movie_before = get_total_movies_count('movies.db')
 
     insert_movies('movies.db', data)
 
@@ -66,9 +36,6 @@ def recommand_data(config, round):
     print(f'Total movie count: {total_movie_after}')
     print(f'Replaced movies count: {replaced_movie_count}')
     print(f'Response total:{data['total']}')
-
-    conn.commit()
-    conn.close()
 
     print(f"{received_count} Data has been inserted into the SQLite database.")
 
